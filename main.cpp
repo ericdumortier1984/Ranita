88b2a9158d91dc0d25387353724ae773472054ad
ericdumortier1984 <ericdumortier1984@gmail.com>
2023-10-03 06:47:05 -0300

    realice clases autito y camion

--- main.cpp
+++ main.cpp
@@ -3,24 +3,122 @@
 #include<windows.h>
 using namespace std;
 
+const int xMax = 79;
+const int xMin = 3;
+const int yMax = 24;
+const int yMin = 2;	
+	
 class Rana{
 protected:
 	int x, y;
-	
-};
-
-class Juego : public Rana {
+	void borrar();
+	void dibujar();
+private:
+	int velocidadRana = 1000;
 public:
-	Juego();
+	Rana();
+	void moverRana();
+};
+Rana::Rana(){
+	
+}
+void Rana::moverRana(){
+	if (_kbhit()) {
+		borrar();
+		char tecla = _getch();
+		if (tecla == 'a' && x > xMin) { x--; }
+		if (tecla == 'd' && x < xMax) { x++; }
+		if (tecla == 'w' && y > yMin) { y--; }
+		if (tecla == 's' && y < yMax) { y++; }
+	}
+	Sleep(velocidadRana);
+}
+void Rana::dibujar(){
+	gotoxy(x, y);
+	x = 40;
+	y = 24;
+	textcolor(GREEN);
+	cout << "0";
+}
+void Rana::borrar(){
+	gotoxy(x, y);
+	cout << " ";
+}
+class Autito : virtual public Rana{
+protected:
+	int autito = xMin;
+	const int velocidad = 100;
+	void dibujarAutito();
+	void borrarAutito();
+public:
+	Autito();
+	void moverAutito();
+};
+Autito::Autito(){
+	autito = xMin;
+}
+void Autito::dibujarAutito(){
+	gotoxy(autito, 5);
+	textcolor(BLUE);
+	cout << "A";
+}
+void Autito::moverAutito(){
+	while(true){
+	borrarAutito();
+	autito++;
+	if (autito > xMax){
+		autito = xMin;
+	}
+	dibujarAutito();
+	Sleep(velocidad);
+	}
+}
+void Autito::borrarAutito(){
+	gotoxy(autito, y);
+	cout << " ";
+}
+class Camion : virtual public Rana {
+protected:
+	int camion = xMax;
+	const int velocidad = 100;
+	void dibujarCamion();
+	void borrarCamion();
+public:
+	Camion();
+	void moverCamion();
+};
+Camion::Camion(){
+	camion = xMax;
+}
+
+void Camion::moverCamion(){
+	while(true){
+	borrarCamion();
+	camion--;
+	if (camion < xMin){
+		camion = xMax;
+	}
+	dibujarCamion();
+	Sleep(velocidad);
+	}
+}
+void Camion::dibujarCamion(){
+	gotoxy(camion, 10);
+	textcolor(RED);
+	cout << "C";
+}
+void Camion::borrarCamion(){
+	gotoxy(camion, y);
+	cout << " ";
+}
+
+class Juego : virtual public Rana, public Camion, public Autito {
+public:
+	Juego() : Rana(), Camion(){}
+	virtual~Juego(){}
 	void Jugar();
 	void marcarBordes();
-	void borrarPosicion();
-	void posicion();
 };
-Juego::Juego(){
-	x = 0;
-	y = 0;
-}
 void Juego::marcarBordes(){
 	for (int i = 1; i < 81; i++){
 		gotoxy(i, 1);
@@ -39,37 +137,17 @@ void Juego::marcarBordes(){
 		cout << "-";
 	}
 }
-void Juego::posicion(){
-	gotoxy(x, y);
-	cout << "0";
-}
-void Juego::borrarPosicion(){
-	gotoxy(x, y);
-	cout << " ";
-}
+
 void Juego::Jugar(){
 	bool juegoActivo = true;
-	int velocidad = 100; // Velocidad del juego en milisegundos
-	int direccion = 0; // Dirección de la rana (0: arriba, 1: derecha, 2: abajo, 3: izquierda)
-	// Inicializar posición de la rana
-	x = 40;
-	y = 23;
 	marcarBordes();
+	Rana R;
+	Camion C;
+	Autito A;
 	while (juegoActivo){
-		if (_kbhit()) {
-			borrarPosicion();
-			char tecla = _getch();
-			if (tecla == 'a') { x--; }
-			if (tecla == 'd') { x++; }
-			if (tecla == 'w') { y--; }
-			if (tecla == 's') { y++; }
-		}
-		posicion();
-		// Verificar condición de finalización del juego
-		if(x < 0 || x >= 80 || y < 0 || y >= 25){ // Si la rana sale de los límites de la pantalla
-			juegoActivo = false; // Finalizar el juego
-		}
-		Sleep(velocidad);
+	R.moverRana();
+	C.moverCamion();
+	A.moverAutito();
 	}
 }
 int main (int argc, char *argv[]) {
@@ -77,4 +155,3 @@ int main (int argc, char *argv[]) {
 	J.Jugar();
 	return 0;
 }
-
